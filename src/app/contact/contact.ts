b746ca287a2a134ac5b50797fafb16db5364d0b1
Flameri <arto.lindgren1@gmail.com>
2018-05-16 14:27:27 +0300

    Added properties for contacts

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,11 +1,23 @@
+import {s, st} from '@angular/core/src/render3';
+
 export class Contact {
   id: number;
+  streetAddress: string;
+  city: string;
+  postalCode: string;
+  phoneNumber: string;
+  emailAddress: string;
   firstName: string;
   lastName: string;
 
 
-  constructor(id?: number, firstName?: string, lastName?: string) {
+  constructor(id?: number,streetAddress?: string,city?: string, postalCode?: string, phoneNumber?: string, emailAddress?: string, firstName?: string, lastName?: string) {
     this.id = id;
+    this.streetAddress = streetAddress;
+    this.city = city;
+    this.postalCode = postalCode;
+    this.phoneNumber = phoneNumber;
+    this.emailAddress = emailAddress;
     this.firstName = firstName;
     this.lastName = lastName;
   }
